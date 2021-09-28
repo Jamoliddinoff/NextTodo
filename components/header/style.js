dc10744289f5eb8d9a074f562e59db3df2ee8df5
Jamoliddinoff <57490115+Jamoliddinoff@users.noreply.github.com>
2021-09-28 10:10:51 +0500

    first commit

---
+++ components/header/style.js
@@ -0,0 +1,9 @@
+ul{
+
+}
+.container{
+    width:60px;
+    background: #555050;
+    margin:0px;
+    width:100%
+}
---
+++ pages/users/index.js
@@ -0,0 +1,12 @@
+import Container from "../components/container/container";
+
+const Blogs = ( ) =>{
+    return(
+        <Container>
+            <h1>
+                Blogs section
+            </h1>
+        </Container>
+    );
+}
+export  default Blogs; 
